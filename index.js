ea3a0430c586aaf05e7eff3f52a61d90177ce54f
tazmul-dev <mdimrahossan146@gmail.com>
2026-05-23 09:45:05 +0600

    make pets route and pets details route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -26,10 +26,32 @@ const run = async () =>{
     const db = client.db('petzone')
     const petCollection = db.collection('pets')
 
+
+
     app.get('/pets', async (req, res)=>{
-       const cursor = petCollection.find()
-       const result = await cursor.toArray()
+      const search = req.query.search;
+      let query ={};
+
+      if(search){
+        query={
+           petName: {
+            $regex: search,
+            $options: "i",
+          }
+        }
+      }
+      const result = await petCollection.find(query).toArray()
        res.send(result)
+    })
+    app.get('/pets/:id', async(req, res)=>{
+      const id = req.params.id
+      console.log(id)
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const petId = await petCollection.findOne(query)
+      // console.log(petId)
+      res.send(petId)
     })
 
 
